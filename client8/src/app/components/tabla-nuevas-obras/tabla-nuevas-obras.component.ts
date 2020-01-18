2a27470d86423dd3ded437dbf02f2f064ae852d5
Lorenzo <lorenzopemac@gmail.com>
2020-01-18 02:54:42 +0100

    Cambio diseño de obraEstado y tablas Home
    mejora en las tablas de obraEstado y del home

--- client8/src/app/components/tabla-nuevas-obras/tabla-nuevas-obras.component.ts
+++ client8/src/app/components/tabla-nuevas-obras/tabla-nuevas-obras.component.ts
@@ -16,7 +16,7 @@ export class TablaNuevasObrasComponent implements AfterViewInit, OnInit {
   dataSource: TablaNuevasObrasDataSource;
 
  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
- displayedColumns = ['id', 'Denominacion','Localidad','Sector'];
+ displayedColumns = ['ver', 'Denominacion','Localidad','Sector'];
  
   ngOnInit() {
     this.dataSource = new TablaNuevasObrasDataSource();
