da699c326a640347911672a23fb9fcc6028ce566
Lorenzo <lorenzopemac@gmail.com>
2020-02-10 00:04:59 -0300

    correcciones de map

--- client8/src/app/components/mapa/mapa.component.ts
+++ client8/src/app/components/mapa/mapa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+//import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 /*import 'leaflet-routing-machine';
 import 'leaflet';
